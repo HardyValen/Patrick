36f00f302c74bd98646e0ecc3a5b9569f4fa47f3
Hardy Valenthio <hardyvalen-db12-0038@localhost.localdomain>
2025-08-10 22:23:15 +0700

    milestone 1 client navbar - todo: drawer

--- src/stories/Composite/ClientNavBar/ClientNavBar.svelte
+++ src/stories/Composite/ClientNavBar/ClientNavBar.svelte
@@ -2,67 +2,64 @@
 
 <script>
   import * as NavigationMenu from "$lib/components/ui/navigation-menu/index.js";
+  import { Separator } from "$lib/components/ui/separator/index.js";
   import Patrick from '$lib/assets/PatrickLogo.svelte';
   import Button from "$sb/Example/Button/Button.svelte";
   import { mode } from "mode-watcher";
   import { cn } from "$lib/utils.js";
   import DarkMode from "$sb/Composite/DarkMode/DarkMode.svelte";
   import { navigationMenuTriggerStyle } from "$lib/components/ui/navigation-menu/navigation-menu-trigger.svelte";
-
-  // let {
-  //   classes = [],
-  // } = $props();
-  let headerClasses = [
-    "w-full",
-    "flex",
-    "justify-between",
-    // "md:justify-evenly",
-    "p-2",
-    "md:p-3",
-    "shadow-xl"
-  ]
-
-  let logoWidth = '160px';
 </script>
 
-<header class={cn(...headerClasses)}>
+<header class={cn("w-full")}>
   <NavigationMenu.Root viewport={false}>
-    <NavigationMenu.List>
-      <NavigationMenu.Item>
-        <a href="/" class={["block", `w-[${logoWidth}]`, "p-2"].join(' ')}>
-          <Patrick mode={mode.current}/>
-        </a>
-      </NavigationMenu.Item>
-    </NavigationMenu.List>
-  </NavigationMenu.Root>
+    {#snippet child()}
+      <div class={cn([
+          "flex",
+          "justify-between",
+          "md:justify-evenly",
+          "p-2",
+          "md:p-3",
+          "w-screen"
+        ].join(' '))}>
+        <NavigationMenu.List>
+          <NavigationMenu.Item>
+            <NavigationMenu.Link>
+              {#snippet child()}
+                <a href="/" class={cn(["block", "w-[160px]", "p-2"].join(' '))}>
+                  <Patrick mode={mode.current}/>
+                </a>
+              {/snippet}
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
+        </NavigationMenu.List>
 
-  <NavigationMenu.Root viewport={false} classes={cn(["hidden"].join())}>
-    <NavigationMenu.List>
-      <NavigationMenu.Item>
-        <NavigationMenu.Link>
-          {#snippet child()}
-           <a href="/docs" class={navigationMenuTriggerStyle()}>Docs</a>
-          {/snippet}
-        </NavigationMenu.Link>
-        <NavigationMenu.Link>
-          {#snippet child()}
-           <a href="/docs" class={navigationMenuTriggerStyle()}>Docs</a>
-          {/snippet}
-        </NavigationMenu.Link>
-        <NavigationMenu.Link>
-          {#snippet child()}
-           <a href="/docs" class={navigationMenuTriggerStyle()}>Docs</a>
-          {/snippet}
-        </NavigationMenu.Link>
-      </NavigationMenu.Item>
-    </NavigationMenu.List>
-  </NavigationMenu.Root>
+        <NavigationMenu.List class={cn(['h-full', 'max-md:hidden'].join(' '))}>
+          <NavigationMenu.Item>
+            <NavigationMenu.Link>
+              {#snippet child()}
+              <a href="/about" class={navigationMenuTriggerStyle()}>Perusahaan</a>
+              {/snippet}
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
+          <NavigationMenu.Item>
+            <NavigationMenu.Link>
+              {#snippet child()}
+              <a href="/docs" class={navigationMenuTriggerStyle()}>Hubungi Kami</a>
+              {/snippet}
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
+        </NavigationMenu.List>
 
-  <NavigationMenu.Root viewport={false}>
-    <NavigationMenu.List class={cn([`md:w-[${logoWidth}]`, "md:justify-end"].join(' '))}>
-      <NavigationMenu.Item>
-        <DarkMode />
-      </NavigationMenu.Item>
-    </NavigationMenu.List>
+        <NavigationMenu.List class={cn(["md:w-[160px]", "md:justify-end", "h-full"].join(' '))}>
+          <NavigationMenu.Item>
+            <DarkMode />
+          </NavigationMenu.Item>
+        </NavigationMenu.List>
+      </div>
+    {/snippet}
   </NavigationMenu.Root>
 </header>
+<Separator
+  class={cn("bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-[1px]")}
+/>
