6190a9d8a0656da80c9f8afa1a24875fab0fee63
HardyValen <hardyvalen@gmail.com>
2025-11-11 10:07:23 +0700

    test gh page

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,15 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+  kit: {
 		adapter: adapter({
-		  out: 'build_node',
+			fallback: '404.html'
 		}),
-		alias: {
-			'$appcss': "/src/app.css",
-			'$data': "/src/data_definitions",
-			'$config': "/src/config"
+		paths: {
+			base: process.argv.includes('dev') ? '' : "/Patrick"
 		}
 	}
-};
+ };
 
-export default config;
+ export default config;
