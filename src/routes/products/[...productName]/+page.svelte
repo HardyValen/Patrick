791f4d217741cebb19394da0647a6b79e4d8d258
HardyValen <hardyvalen@gmail.com>
2026-04-04 15:14:47 +0700

    release candidate for 0005

--- src/routes/products/[...productName]/+page.svelte
+++ src/routes/products/[...productName]/+page.svelte
@@ -2,7 +2,8 @@
   import { cn } from "$lib/utils.js";
   import { typographyVariants } from "$lib";
   import { FeaturedProductTop, FeaturedProductFooter, Meta, ProductCarousel, Taglist } from "$lib/composite";
-  import * as Accordion from "$lib/components/ui/accordion";
+  import { Button } from "$lib/components/ui/button";
+    import { ExternalLink } from "@lucide/svelte";
 
   let {
     ref = $bindable(null),
@@ -11,11 +12,12 @@
     ...restProps
   } = $props();
 
-  const { current } = $derived(productData ?? {});
+  const { current, meta } = $derived(productData ?? {});
+  let showCompleteDescription = $state(false);
 
 </script>
 
-<Meta metadata={productData.meta}/>
+<Meta metadata={meta}/>
 
 <div
   bind:this={ref}
@@ -24,13 +26,14 @@
   )}
   {...restProps}
 >
-  <!-- Layout for the page -->
+  <!-- LAYOUT FOR THE PAGE -->
   <div class={cn(
     // "border-4 border-red-500",
     "w-full max-w-4xl m-auto",
-    "grid grid-cols-8 gap-2",
+    "grid grid-cols-8 gap-2 md:gap-8",
     "py-4"
   )}>
+    <!-- LAYOUT FOR CAROUSEL -->
     <div class={cn(
       // "border-4 border-blue-500",
       "col-span-8 md:col-span-3"
@@ -42,65 +45,115 @@
       "col-span-8 md:col-span-5",
       "px-4 md:p-0"
     )}>
-      <!-- CURRENCY -->
-      <p>
-        <span class="text-lg font-semibold">
-          Rp
-        </span>
-        <span class="font-extrabold text-2xl md:text-3xl">
-          {Intl.NumberFormat("id-ID", {currency:"IDR", minimumFractionDigits:0, maximumFractionDigits:0}).format(current.retail_price_idr)}
-        </span>
-      </p>
-      <!-- END OF CURRENCY -->
-      <!-- END OF PRODUCT NAME -->
-      <p
-        class={cn(
-          "font-extrabold text-lg md:text-xl"
-        )}
-      >
-        {current.name}
-      </p>
-      <!-- PRODUCT NAME -->
-      <!-- TAGLIST -->
-      <Taglist
-        class="my-2"
-        tags={current.tags.map(tag => {return {title: tag, href:`/products?tags=${tag}`}})}
-        itemClass={cn(
-          "border-1 border-slate-600 dark:border-slate-400",
-          "text-[9px] md:text-[10px]",
-          "text-slate-600 dark:text-slate-400",
-          "transition hover:bg-slate-600 hover:text-white dark:hover:bg-slate-400 hover:dark:text-white"
-        )}
-      />
-      <!-- END OF TAGLIST -->
-      <!-- PRODUCT -->
       <div class={cn(
-        "mt-2"
+        // "border-4 border-green-500",
+        "grid grid-cols-1 gap-8"
       )}>
-        <Accordion.Root type="multiple">
-          <Accordion.Item value="item-1">
-            <Accordion.Trigger>
-              <h2 class={cn(
-                "font-extrabold text-xl md:text-2xl"
-              )}>
-                Tentang Produk
-              </h2>
-            </Accordion.Trigger>
-            <Accordion.Content>
-              {@html current.description}
-            </Accordion.Content>
-          </Accordion.Item>
-        </Accordion.Root>
+        <!-- PRODUCT DESCRIPTION HEADER -->
+        <div class={cn(
+          // "border-4 border-fuchsia-500",
+        )}>
+          <!-- CURRENCY -->
+          <div class={cn(
+            // "border-4 border-cyan-500"
+          )}>
+            <span class="text-lg font-semibold">
+              Rp
+            </span>
+            <span class="font-extrabold text-2xl md:text-3xl">
+              {Intl.NumberFormat("id-ID", {currency:"IDR", minimumFractionDigits:0, maximumFractionDigits:0}).format(current.retail_price_idr)}
+            </span>
+          </div>
+          <!-- END OF CURRENCY -->
+          <!-- PRODUCT NAME -->
+          <div
+            class={cn(
+              // "border-4 border-cyan-500",
+              "font-extrabold text-lg md:text-xl"
+            )}
+          >
+            {current.name}
+          </div>
+          <!-- END OF PRODUCT NAME -->
+          <!-- TAGLIST -->
+          <Taglist
+            class={cn(
+              // "border-4 border-cyan-500"
+              "mt-2"
+            )}
+            tags={current.tags.map(tag => {return {title: tag, href:`/products?tags=${tag}`}})}
+            itemClass={cn(
+              "border-1 border-slate-600 dark:border-slate-400",
+              "text-[9px] md:text-[10px]",
+              "text-slate-600 dark:text-slate-400",
+              "transition hover:bg-slate-600 hover:text-white dark:hover:bg-slate-400 hover:dark:text-white"
+            )}
+          />
+          <!-- END OF TAGLIST -->
+        </div>
+        <!-- END OF PRODUCT DESCRIPTION HEADER -->
+        <!-- EXTERNAL LINKS -->
+        <div class={cn(
+          // "border-4 border-fuchsia-500",
+          "flex flex-row items-start",
+          "gap-2"
+        )}>
+          <Button
+            class={cn(
+              "cursor-pointer"
+            )}
+            href={current.url_tokopedia}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Buy at Tokopedia
+            <ExternalLink/>
+          </Button>
+          <Button
+            variant="secondary"
+            class={cn(
+              "cursor-pointer"
+            )}
+            href={current.url_tiktok_shop}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Buy at TikTok Shop
+            <ExternalLink/>
+          </Button>
+        </div>
+        <!-- END OF EXTERNAL LINKS -->
+        <!-- PRODUCT DESCRIPTION BODY -->
+        <div class={cn(
+          // "border-4 border-fuchsia-500",
+          "flex flex-col",
+          "gap-2"
+        )}>
+          <div class={cn(
+            "font-extrabold text-xl md:text-2xl",
+          )}>
+            Tentang Produk
+          </div>
+          <div class={cn(
+            !showCompleteDescription && "line-clamp-5",
+          )}>
+            {@html current.description}
+          </div>
+          <div class={cn(
+            // "border-4 border-red-500",
+            "flex justify-end"
+          )}>
+            <Button variant="link" class="cursor-pointer" onclick={() => {
+              showCompleteDescription = !showCompleteDescription;
+            }}>
+              {showCompleteDescription ? "Show Less" : "Show More"}
+            </Button>
+          </div>
+        </div>
+        <!-- END OF PRODUCT DESCRIPTION BODY -->
       </div>
-      {@html current.description}
-      <!-- {current.url_tiktok_shop} -->
-      <!-- {current.url_tokopedia} -->
-    </div>
-    <!-- <div class={cn(
-      "border-4 border-purple-500",
-      "max-xl:hidden md:col-span-2"
-    )}>
 
-    </div> -->
+    </div>
   </div>
+  <!-- END OF MAIN LAYOUT -->
 </div>
